7b037453d76b8c60c19c002777556a97b333757b
nishiiko <142626254+nishiiko@users.noreply.github.com>
2025-03-21 19:54:02 +0000

    Add files via upload

--- .config/sway/extra/pipewire.sh
+++ .config/sway/extra/pipewire.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 
+killall pipewire pipewire-pulse wireplumber ;
+
 pipewire &
 pipewire-pulse &
 wireplumber &
 
-paplay --volume=65535 $HOME/.config/sway/extra/startup.wav &
+# paplay --volume=65535 $HOME/.config/hypr/extra/startup.wav &
