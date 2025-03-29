bd97e3829a2d05bb0e55e8cf3934b003f012b760
nishiiko <142626254+nishiiko@users.noreply.github.com>
2025-03-29 22:42:09 +0000

    Delete .config/sway/extra/xdpw.sh

--- .config/sway/extra/xdpw.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-sleep 1
-killall -e xdg-desktop-portal-wlr
-killall xdg-desktop-portal
-/usr/libexec/xdg-desktop-portal-wlr &
-/usr/libexec/xdg-desktop-portal-gtk &
-sleep 2
-/usr/libexec/xdg-desktop-portal &
