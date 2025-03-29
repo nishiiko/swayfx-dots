24f86e04d8378c424f0c06b0687b8f2ad329b43d
nishiiko <142626254+nishiiko@users.noreply.github.com>
2025-03-29 14:56:31 +0000

    Add files via upload

--- .config/sway/extra/scripts/opacity.sh
+++ .config/sway/extra/scripts/opacity.sh
@@ -7,10 +7,9 @@ while true; do
 	new=$(swaymsg -t subscribe '["window"]' | jq '.. | select(.type?) | .id')
 	if [[ $new -eq $old ]]; then
 		continue
-	fi
-	if [[ $old -ne 0 ]]; then
+    else	
 		swaymsg [con_id = $old] opacity set 0.85
 	fi
-	swaymsg [con_id = $new] opacity set 0.95
+    swaymsg [con_id = $new] opacity set 0.95
 	old=$new
 done
