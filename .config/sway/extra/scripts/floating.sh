71f4d9f29280fffc3cd88dac70bc6ed09bd70aff
nishiiko <nishiiko@protonmail.com>
2025-03-30 04:12:05 +0100

    modified:   .config/sway/extra/scripts/floating.sh

--- .config/sway/extra/scripts/floating.sh
+++ .config/sway/extra/scripts/floating.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
 while true; do
-	read -r windowID floatingState <<< $(swaymsg -t subscribe '["window"]' | jq '.. | select(.type?) | .id, .type' | tr '\n' ' ')
-
+    read -r windowID floatingState <<< $(swaymsg -t subscribe -m '["window"]' | jq '.. | select(.type?) | .id, .type' | tr '\n' ' ')
     if [[ $floatingState == '"con"' ]]; then
 		swaymsg [con_id = $windowID] border normal 4
     fi
-	if [[ "$floatingState" == '"floating_con"' ]]; then
-		swaymsg [con_id = $windowID] border pixel 4
-	fi
+    if [[ "$floatingState" == '"floating_con"' ]]; then
+        swaymsg [con_id = $windowID] border pixel 4
+    fi
 
     if pgrep sway &>/dev/null; then
         continue
