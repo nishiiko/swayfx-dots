55f804d7894acb421467ee45701fd1ab144be3ec
nishiiko <nishiiko@protonmail.com>
2025-03-31 10:59:12 +0100

    modified:   .config/sway/extra/scripts/opacity.sh

--- .config/sway/extra/scripts/opacity.sh
+++ .config/sway/extra/scripts/opacity.sh
@@ -7,7 +7,7 @@ while read -r line; do
     if [[ $old -eq $new ]] || [[ ! $visible == 'true' ]] || [[ ! $focus == true ]]; then
         continue
     else	
-		swaymsg [con_id = $old] opacity set 0.75
+		swaymsg [con_id = $old] opacity set 0.8
 	fi
     
     swaymsg [con_id = $new] opacity set 0.9
