b2a4f239a1d69821b65d0a617da3799a8a88a625
nishiiko <nishiiko@protonmail.com>
2025-03-31 12:27:22 +0100

    modified:   .config/sway/config.d/windowrules 	modified:   .config/sway/extra/scripts/opacity.sh

--- .config/sway/extra/scripts/opacity.sh
+++ .config/sway/extra/scripts/opacity.sh
@@ -4,6 +4,9 @@ old=0
 while read -r line; do
     read -r new visible focus <<< $(echo "$line" | jq '.. | select(.type?) | .id, .visible, .focused' | tr '\n' ' ')
 
+    echo $old $new
+    echo $visible $focus
+
     if [[ $old -eq $new ]] || [[ ! $visible == 'true' ]] || [[ ! $focus == true ]]; then
         continue
     else	
