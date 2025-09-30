c58af188710668c15ba654f17ec73ee08786cfbc
nishiiko <nishiiko@protonmail.com>
2025-09-30 11:16:18 +0100

    vpn module 	modified:   .config/waybar/scripts/vpn.sh

--- .config/waybar/scripts/vpn.sh
+++ .config/waybar/scripts/vpn.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ip=$(curl -s https://icanhazip.com)
 
-ls /proc/sys/net/ipv4/conf/*mullvad* &>/dev/null \ # only works with mullvad vpn, adjust as needed
+# only works with mullvad, adjust as needed
+ls /proc/sys/net/ipv4/conf/*mullvad* &>/dev/null \
 && jq -c -n --arg ip "$ip" '{"text":$ip,"class":"connected","percentage":100}' \
 || echo '{"text":"VPN Disconnected","class":"disconnected","percentage":0}'
