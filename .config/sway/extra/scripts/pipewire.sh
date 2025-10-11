b7926cdf3568decd120ca2cad1a902318b759896
nishiiko <nishiiko@protonmail.com>
2025-10-11 14:43:44 +0100

    modified:   .config/sway/config.d/keybinds 	modified:   .config/sway/extra/scripts/pipewire.sh

--- .config/sway/extra/scripts/pipewire.sh
+++ .config/sway/extra/scripts/pipewire.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-if ! grep -r systemd /sbin/init; then
-    pkill -x pipewire\|pipewire-pulse\|wireplumber
+## openrc supports user services now, i dont need to use this :>
 
-    pidwait -x pipewire\|pipewire-pulse\|wireplumber
-    
-    pipewire &
-    pipewire-pulse &
-
-    sleep 1s
-    wireplumber &
-fi
-
-sleep 0.5s
+# if ! grep -r systemd /sbin/init; then
+#     pkill -x pipewire\|pipewire-pulse\|wireplumber
+# 
+#     pidwait -x pipewire\|pipewire-pulse\|wireplumber
+#     
+#     pipewire &
+#     pipewire-pulse &
+# 
+#     sleep 1s
+#     wireplumber &
+# fi
 
 paplay --volume=65535 $HOME/.config/sway/extra/assets/heal.wav &
