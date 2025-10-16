3b6cfd32dd28779ffed457846ca6219de40fc90c
nishiiko <nishiiko@protonmail.com>
2025-10-16 12:07:13 +0100

    modified:   .config/waybar/config.jsonc 	modified:   .config/waybar/scripts/vpn.sh

--- .config/waybar/scripts/vpn.sh
+++ .config/waybar/scripts/vpn.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-ip=$(curl -s https://icanhazip.com)
+
+strip_all () {
+    printf '%s\n' "${1//$2}"
+}
 
 # only works with mullvad, adjust as needed
-mullvad status | grep "Connected" &>/dev/null \
+mullvad status | grep -e "Connected" -e "Connecting" &>/dev/null \
+&& ip=$(curl -s4 ipinfo.io | jq -r '"\(.ip) \(.country)"') \
+&& ip=$(strip_all "$ip" "[\"]") \
 && jq -c -n --arg ip "$ip" '{"text":$ip,"class":"connected","percentage":100}' \
 || echo '{"text":"VPN Disconnected","class":"disconnected","percentage":0}'
