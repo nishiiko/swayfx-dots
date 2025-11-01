d4f036f2452e09a664127c563d7d2d9bc06bd96a
nishiiko <nishiiko@protonmail.com>
2025-11-01 20:10:04 +0000

    modified:   README.md 	modified:   install.sh

--- README.md
+++ README.md
@@ -4,11 +4,16 @@ yell at me in issues if something no worky
 
 ## some info
 gentoo linux\
-openrc
+openrc\
+
+theres a theme switcher in ~/.config/sway/extra/scripts/themeswap.sh\
+ill make a rofi menu instead when i eventually get too annoyed\
+at manually invoking the script
 
 ## things you need (maybe):
 swayfx (duh)\
-swww\
+swww (purple theme)\
+mpvpaper (green theme)\
 swaync\
 foot\
 rofi (+ rofi emoji)\
--- install.sh
+++ install.sh
@@ -9,10 +9,17 @@ i dont recommend you actually use this, i made this
 for myself where i intend to always nuke my previous config files
 please backup your current dots first
 
-i highly advise you edit .config/sway/config.d/monitors before
-using the install script, otherwise you may be left with a black
-screen if my configs do not match exactly what your monitor(s) can
-handle
+you should probably edit $PWD/.config/sway/config.d/monitors before
+using the install script
+
+BIG LETTERS SO YOU KNOW THIS IS IMPORTANT
+IT WILL RM -RF THESE DIRECTORIES
+$HOME/.config/fastfetch
+$HOME/.config/foot
+$HOME/.config/rofi
+$HOME/.config/sway
+$HOME/.config/swaync
+$HOME/.config/waybar
 
 are you sure you want to run this (y/n): " confirm
 
@@ -30,6 +37,13 @@ if [ $confirm == "y" ]; then
         sleep 1
         : $((countdown--))
     done
+
+    rm -rf $HOME/.config/fastfetch
+    rm -rf $HOME/.config/foot
+    rm -rf $HOME/.config/rofi
+    rm -rf $HOME/.config/sway
+    rm -rf $HOME/.config/swaync
+    rm -rf $HOME/.config/waybar
     
     cp -rv --preserve=links .config/* $HOME/.config
     cp -rv --preserve=links .local/* $HOME/.local
