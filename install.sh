751ffe74b4e45c1044821febdf5b4e403a67930d
nishiiko <nishiiko@protonmail.com>
2025-12-18 12:17:01 +0000

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -53,7 +53,7 @@ if [ $confirm == "y" ]; then
     swaync-client -rs &
     swaymsg reload
     $HOME/.config/sway/extra/scripts/wallpaper.sh & disown
-    pkill waybar ; waybar & disown
+    pkill -x waybar ; waybar & disown
     echo
     echo done
 fi
