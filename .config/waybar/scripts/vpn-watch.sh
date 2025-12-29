d656062a908e0a4d3d61db1cee581860f34ffdca
nishiiko <nishiiko@protonmail.com>
2025-12-29 22:36:08 +0000

    modified:   .config/waybar/scripts/vpn-watch.sh 	modified:   .config/waybar/scripts/vpn.sh

--- .config/waybar/scripts/vpn-watch.sh
+++ .config/waybar/scripts/vpn-watch.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+lastline=
+
 while read -r line; do
+  if [[ $lastline == $line ]] \
+  || echo $line | grep Deleted &>/dev/null; then
     pkill -RTMIN+8 waybar
+  fi
+  if echo $line | grep mullvad &>/dev/null; then
+    lastline=$(echo $line)
+  fi
 done < <(ip monitor link)
--- .config/waybar/scripts/vpn.sh
+++ .config/waybar/scripts/vpn.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/env bash
 
 vpn_ip=
+connected=false
+sleep 0.2s
 
-if ! mullvad status | grep -q -e "Connecting" -e "Connected"; then
-    echo '{"text":"VPN Disconnected","class":"disconnected","percentage":0}'
-    exit
+if ! mullvad status | grep -q "Connect"; then
+  echo '{"text":"VPN Disconnected","class":"disconnected","percentage":0}'
+  exit
 fi
 
-until mullvad status | grep -q "Connected"; do
-    continue
+for i in {1..100}; do
+  if mullvad status | grep -q "Connected"; then
+    connected=true
+    break 1
+  fi
+  sleep 0.1s
+  continue
 done
+
+if ! $connected; then
+  echo '{"text":"VPN Disconnected","class":"disconnected","percentage":0}'
+  exit
+fi
 
 vpn_ip=$(curl -s4 ipinfo.io | jq -r '"\(.ip) \(.country)"')
 if [[ -n "$vpn_ip" ]]; then
-    jq -c -n --arg ip "$vpn_ip" '{"text":$ip,"class":"connected","percentage":100}'
+  jq -c -n --arg ip "$vpn_ip" '{"text":$ip,"class":"connected","percentage":100}'
 fi
