2a46eb1c16f261edf9443c8ce71212c4d56f03d3
nishiiko <nishiiko@protonmail.com>
2026-01-18 23:30:52 +0000

    modified:   .config/sway/extra/scripts/floating.sh

--- .config/sway/extra/scripts/floating.sh
+++ .config/sway/extra/scripts/floating.sh
@@ -5,9 +5,9 @@ while read -r line; do
     read -r windowID floatingState <<< $(echo "$line" | jq '.. | select(.type?) | .id, .type' | tr '\n' ' ')
 
     if [[ $floatingState == '"con"' ]]; then
-        swaymsg [con_id = $windowID] border pixel 6
+        swaymsg [con_id = $windowID] border normal 6
     elif [[ $floatingState == '"floating_con"' ]]; then
-        swaymsg [con_id = $windowID] border pixel 5
+        swaymsg [con_id = $windowID] border pixel 6
     fi;
     
 done < <(swaymsg -m -t subscribe '[ "window" ]')
