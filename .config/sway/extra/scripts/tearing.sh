702ac619824b9e79995c42c3d9950cd43d3366b0
nishiiko <nishiiko@protonmail.com>
2026-01-26 04:00:47 +0000

    modified:   .config/sway/extra/scripts/tearing.sh

--- .config/sway/extra/scripts/tearing.sh
+++ .config/sway/extra/scripts/tearing.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 tearing_state=$(swaymsg -r -t get_outputs | jq '.. | select(.type?) | .allow_tearing')
-
+workspace=$(swaymsg -r -t get_workspaces | jq '.[] | select(.focused==true).name')
 if $tearing_state; then
   swaymsg output '*' allow_tearing no
   notify-send "Tearing Toggle" "Tearing Disabled" -e -a sway-script
